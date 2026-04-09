2422848aad48f097742e41c4b4dd76bff4ab0927
kislly <1562264810@qq.com>
2026-04-09 15:16:41 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     // 主方法：Java程序的入口点，程序从这里开始执行
     public static void main(String[] args) {
         // 控制台输出语句：打印 Hello World!
-        System.out.println("Hello World!");
+
+        System.out.println("Hello New World!");
     }
 }
