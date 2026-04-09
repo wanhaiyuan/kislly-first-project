c1674c51b041ef262854fe2c5d18b1b9b8fd5390
kislly <1562264810@qq.com>
2026-04-09 15:20:39 +0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    // 主方法：Java程序的入口点，程序从这里开始执行
-    public static void main(String[] args) {
-        // 控制台输出语句：打印 Hello World!
-
-        System.out.println("Hello New World!");
-    }
-}
